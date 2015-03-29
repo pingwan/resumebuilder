188796b1e04c2d05b74cee4805b36a8951d83414
Roeland Oosterloo <roeland.1992@gmail.com>
2015-03-29 14:58:31 +0200

    api providing the possibility to post data to crowdflower

---
+++ crowdflower/index.js
@@ -0,0 +1,24 @@
+var request = require('request');
+var baseUrl = "https://api.crowdflower.com/v1/";
+
+
+API_KEY = "xq8tGbMQtwno8Sg5J3E1";
+job_id = "707935";
+
+var postTask = function(task, callback) {
+    var url = baseUrl + "jobs/" + job_id + "/units.json"
+
+    request
+        .post(url, {form:{
+            key: API_KEY,
+            unit: {
+                data: task,
+                state: 'new'
+            }
+        }},callback);
+};
+
+
+module.exports = {
+    postTask: postTask
+};
