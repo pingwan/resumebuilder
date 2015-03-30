f932716b05aa0f3ab3ef7f281fb4056a22b0e8a5
Roeland Oosterloo <roeland.1992@gmail.com>
2015-03-30 10:39:58 +0200

    update job code

--- crowdflower/index.js
+++ crowdflower/index.js
@@ -3,7 +3,7 @@ var baseUrl = "https://api.crowdflower.com/v1/";
 
 
 API_KEY = "xq8tGbMQtwno8Sg5J3E1";
-job_id = "707935";
+job_id = "708538";
 
 var postTask = function(task, callback) {
     var url = baseUrl + "jobs/" + job_id + "/units.json"
